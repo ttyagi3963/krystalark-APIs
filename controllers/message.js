e86700419085ebfe1fdb34beebd48915a9ae85fd
ttyagi3963 <tarun.tyagi1978@gmail.com>
2019-07-11 17:46:48 -0700

    added beneficiary to user

--- controllers/message.js
+++ controllers/message.js
@@ -35,8 +35,42 @@ exports.createMessage = (req, res, next)=>{
                               })
                               return message.save()
                                             .then(messageData =>{
-                                            res.status(200).json({status:200, messageData: messageData})
+                                                User.updateOne
+                                                    (
+                                                        {_id: req.userId},
+                                                        { 
+                                                            $addToSet:{beneficiarys:  bId, messages: messageData._id }
+                                                        }
+                                                    )
+                                                    .then(result => {
+                                                                                                    
+                                                    })
+                                                    .catch(err =>{
+                                                         if(!err.statusCode){
+                                                                err.statusCode = 500;
+                                                            }
+                                                            error.message ="was not able to update to user"
+                                                            next(err);
+                                                     User.updateOne
+                                                            (
+                                                                {_id: req.userId},
+                                                                { 
+                                                                    $addToSet:{beneficiarys:  bId, messages: messageData._id }
+                                                                }
+                                                            )
+                                                            .then(result => {
+                                                                res.status(200).json({status:200, messageData: messageData})
+ 
+                                                            })
+                                                            .catch(err =>{
+                                                                    if(!err.statusCode){
+                                                                        err.statusCode = 500;
+                                                                    }
+                                                                    error.message ="was not able to update to user"
+                                                                    next(err);
+                                                            })})
 
+                                           
                                       })
                                       .catch(err =>{
                                               if(!err.statusCode){
